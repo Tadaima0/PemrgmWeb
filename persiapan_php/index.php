c1bab84dbcd721a0e4b72715b8131afdee576722
Tadaima0 <162398980+Tadaima0@users.noreply.github.com>
2024-06-05 20:10:19 +0700

    Add files via upload

---
+++ persiapan_php/index.php
@@ -0,0 +1,11 @@
+<?php
+// $username = $_POST['username'];
+// $password = $_POST['password'];
+
+// echo $username. ' '. $password;
+// foreach ($_POST as $key => $value) {
+//     echo  $value;
+// }
+foreach ($_POST as $datauser) {
+    echo ' '. $datauser;
+}
---
+++ persiapan_php/login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head>
+    <title>Form login PHP</title>
+</head>
+<body>
+    <h4>login</h4>
+<form action="index.php" method="post">
+    <input type="text" name="username" placeholder="username"/>
+    <input type="password" name="password" placeholder="password"/>
+    <button type="submit" name="klik ">kirim</button>
+</form>    
+</body>
